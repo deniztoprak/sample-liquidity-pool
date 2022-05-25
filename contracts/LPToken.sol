045200a39479916c988906e50af3453a57d46961
Deniz Toprak <deniztoprak@users.noreply.github.com>
2022-05-25 16:53:39 +0000

    Remove redundant storage

--- contracts/LPToken.sol
+++ contracts/LPToken.sol
@@ -5,8 +5,6 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract LPToken is ERC20 {
-    string private greeting;
-
     constructor(uint256 initialSupply) ERC20("LPToken", "LP") {
         _mint(msg.sender, initialSupply);
     }
