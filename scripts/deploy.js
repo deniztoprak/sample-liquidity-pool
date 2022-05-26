b3b5727cf73a192125281439e951bf56cb470671
Deniz Toprak <deniztoprak@users.noreply.github.com>
2022-05-26 18:21:56 +0000

    Refactor deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,35 @@
 const hre = require('hardhat');
 
-async function main() {
-  // Deploy LPToken
+async function deployLPToken() {
   const LPToken = await hre.ethers.getContractFactory('LPToken');
   const initialSupply = hre.ethers.BigNumber.from('999999999999999999');
   const LPTokenInstance = await LPToken.deploy(initialSupply);
   await LPTokenInstance.deployed();
   console.log('LPToken deployed to:', LPTokenInstance.address);
 
-  // Deploy RewardToken
+  return LPTokenInstance.address;
+}
+
+async function deployRewardToken() {
   const RewardToken = await hre.ethers.getContractFactory('RewardToken');
   const RewardTokenInstance = await RewardToken.deploy();
   await RewardTokenInstance.deployed();
   console.log('RewardToken deployed to:', RewardTokenInstance.address);
 
-  // Deploy StakeForReward
+  return RewardTokenInstance.address;
+}
+
+async function main() {
+  // Deploy dummy LPToken
+  // Provide LPToken address in production environment
+  const LPTokenAddress = await deployLPToken();
+
+  // Deploy dummy RewardToken
+  // Provide RewardToken address in production environment
+  const RewardTokenAddress = await deployRewardToken();
+
   const StakeForReward = await hre.ethers.getContractFactory('StakeForReward');
-  const StakeForRewardInstance = await StakeForReward.deploy(LPTokenInstance.address, RewardTokenInstance.address);
+  const StakeForRewardInstance = await StakeForReward.deploy(LPTokenAddress, RewardTokenAddress);
   await StakeForRewardInstance.deployed();
   console.log('StakeForReward deployed to:', StakeForRewardInstance.address);
 }
