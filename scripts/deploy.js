83e46bea3d01fa1ea39013ef596933c5c697ee20
Deniz Toprak <deniztoprak@users.noreply.github.com>
2022-05-30 22:52:43 +0000

    Update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,37 +1,34 @@
 const hre = require('hardhat');
 
+let LPTokenInstance;
+let RewardTokenInstance;
+
 async function deployLPToken() {
   const LPToken = await hre.ethers.getContractFactory('LPToken');
   const initialSupply = hre.ethers.BigNumber.from('999999999999999999');
-  const LPTokenInstance = await LPToken.deploy(initialSupply);
+  LPTokenInstance = await LPToken.deploy(initialSupply);
   await LPTokenInstance.deployed();
   console.log('LPToken deployed to:', LPTokenInstance.address);
-
-  return LPTokenInstance.address;
 }
 
 async function deployRewardToken() {
   const RewardToken = await hre.ethers.getContractFactory('RewardToken');
-  const RewardTokenInstance = await RewardToken.deploy();
+  RewardTokenInstance = await RewardToken.deploy('ipfs://123456789/');
   await RewardTokenInstance.deployed();
   console.log('RewardToken deployed to:', RewardTokenInstance.address);
-
-  return RewardTokenInstance.address;
 }
 
 async function main() {
-  // Deploy dummy LPToken
-  // Provide LPToken address in production environment
-  const LPTokenAddress = await deployLPToken();
-
-  // Deploy dummy RewardToken
-  // Provide RewardToken address in production environment
-  const RewardTokenAddress = await deployRewardToken();
+  await deployLPToken();
+  await deployRewardToken();
 
   const StakeForReward = await hre.ethers.getContractFactory('StakeForReward');
-  const StakeForRewardInstance = await StakeForReward.deploy(LPTokenAddress, RewardTokenAddress);
+  StakeForRewardInstance = await StakeForReward.deploy(LPTokenInstance.address, RewardTokenInstance.address);
   await StakeForRewardInstance.deployed();
   console.log('StakeForReward deployed to:', StakeForRewardInstance.address);
+
+  // Set stake contract as the owner of reward token contract
+  await RewardTokenInstance.transferOwnership(StakeForRewardInstance.address);
 }
 
 main()
