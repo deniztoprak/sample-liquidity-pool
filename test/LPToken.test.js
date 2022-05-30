171c39e1134a05e66aeb9d9d356799478540c9df
Deniz Toprak <deniztoprak@users.noreply.github.com>
2022-05-30 22:48:10 +0000

    Add tests for LPToken

---
+++ test/LPToken.test.js
@@ -0,0 +1,14 @@
+const { expect } = require('chai');
+
+describe('LPToken', function () {
+  it.only('mints token on deployment', async function () {
+    const LPToken = await hre.ethers.getContractFactory('LPToken');
+    const initialSupply = hre.ethers.BigNumber.from('999999999999999999');
+    const LPTokenInstance = await LPToken.deploy(initialSupply);
+    await LPTokenInstance.deployed();
+
+    const balance = await LPTokenInstance.totalSupply();
+
+    expect(balance).to.equal(initialSupply);
+  });
+});
