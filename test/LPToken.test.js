827655290c89c81783bd59d37e6da9523511331f
Deniz Toprak <deniztoprak@users.noreply.github.com>
2022-05-31 00:24:07 +0000

    Remove test only operator

--- test/LPToken.test.js
+++ test/LPToken.test.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 
 describe('LPToken', function () {
-  it.only('mints token on deployment', async function () {
+  it('mints token on deployment', async function () {
     const LPToken = await hre.ethers.getContractFactory('LPToken');
     const initialSupply = hre.ethers.BigNumber.from('999999999999999999');
     const LPTokenInstance = await LPToken.deploy(initialSupply);
